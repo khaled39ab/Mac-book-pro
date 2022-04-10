504a2e84b0ceeb4f8c29a6c3bff11883661ca92a
khaled39ab <khaled39ab@gmail.com>
2022-04-10 12:42:54 +0600

    Calculation area done

--- JS/app.js
+++ JS/app.js
@@ -2,33 +2,85 @@ document.addEventListener('input', (e) => {
     const bestPriceText = document.getElementById("best-price");
     const bestPrice = parseInt(bestPriceText.innerText);
 
+    let totalPrice;
+    const totalPriceText = document.getElementById("total-price");
+    totalPriceText.innerText = totalPrice;
+
+    const extraMemoryText = document.getElementById("extra-memory");
+    const extraMemory = parseInt(extraMemoryText.innerText);
+    
+    const extraStorageText = document.getElementById("extra-storage");
+    const extraStorage = parseInt(extraStorageText.innerText);
+
+    const deliveryChargeText = document.getElementById("delivery-charge");
+    const deliveryCharge = parseInt(deliveryChargeText.innerText);
+
+    const grandTotalText = document.getElementById("grand-total");
+    const grandTotal = parseInt(grandTotalText.innerText);
+
     if (e.target.getAttribute('name') == "memory") {
         if (e.target.value == 2) {
-            const extraMemoryText = document.getElementById("extra-memory");
             const extraMemoryCost = 180;
             extraMemoryText.innerText = extraMemoryCost;
 
-            const totalPrice = document.getElementById("total-price");
-            totalPrice.innerText = bestPrice + extraMemoryCost;
-        } else{
-            const extraMemoryText = document.getElementById("extra-memory");
-            const extraMemoryCost = 0;
-            extraMemoryText.innerText = extraMemoryCost;
+            totalPrice = bestPrice + extraMemoryCost + extraStorage + deliveryCharge;
+            totalPriceText.innerText = totalPrice;
 
-            const totalPrice = document.getElementById("total-price");
-            totalPrice.innerText = bestPrice + extraMemoryCost;
+            grandTotalText.innerText = totalPrice;
+        } else {
+            const memoryCost = 0;
+            extraMemoryText.innerText = memoryCost;
+
+           totalPrice = bestPrice + memoryCost + extraStorage + deliveryCharge;
+           totalPriceText.innerText = totalPrice;
+
+           grandTotalText.innerText = totalPrice;
         }
     }
     else if (e.target.getAttribute('name') == "storage") {
         if (e.target.value == 2) {
-            document.getElementById("extra-storage").innerText = 100;
+            const extraStorageCost = 100;
+            extraStorageText.innerText = extraStorageCost;
+
+            totalPrice = bestPrice + extraStorageCost + extraMemory + deliveryCharge;
+            totalPriceText.innerText = totalPrice;
+
+            grandTotalText.innerText = totalPrice;
         } else if (e.target.value == 3) {
-            document.getElementById("extra-storage").innerText = 180;
+            const extraStorageCost = 180;
+            extraStorageText.innerText = extraStorageCost;
+
+            totalPrice = bestPrice + extraStorageCost + extraMemory + deliveryCharge;
+            totalPriceText.innerText = totalPrice;
+
+            grandTotalText.innerText = totalPrice;
+        } else{
+            const extraStorageCost = 0;
+            extraStorageText.innerText = extraStorageCost;
+
+            totalPrice = bestPrice + extraStorageCost + extraMemory + deliveryCharge;
+            totalPriceText.innerText = totalPrice;
+
+            grandTotalText.innerText = totalPrice;
         }
     }
     else if (e.target.getAttribute('name') == "delivery") {
-        if (e.target.value == 2) {
-            document.getElementById("delivery-charge").innerText = 20;
+        if (e.target.value == 1) {
+            const deliveryCharge = 0;
+            deliveryChargeText.innerText = deliveryCharge;
+
+            totalPrice = bestPrice + extraStorage + extraMemory + deliveryCharge;
+            totalPriceText.innerText = totalPrice;
+
+            grandTotalText.innerText = totalPrice;
+        } else{
+            const deliveryCharge = 20;
+            deliveryChargeText.innerText = deliveryCharge;
+
+            totalPrice = bestPrice + extraStorage + extraMemory + deliveryCharge;
+            totalPriceText.innerText = totalPrice;
+
+            grandTotalText.innerText = totalPrice;
         }
     }
 });
